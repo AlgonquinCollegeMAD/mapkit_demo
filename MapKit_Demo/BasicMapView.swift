ed53d1cc5e4b058f83f63d4d0b6a9da59b9d1aac
Vladimir Cezar <vladimircezar@icloud.com>
2024-11-12 15:22:14 -0500

    Add overlays

--- MapKit_Demo/BasicMapView.swift
+++ MapKit_Demo/BasicMapView.swift
@@ -15,26 +15,26 @@ struct BasicMapView: View {
     Location(name: "National Gallery of Canada", coordinate: CLLocationCoordinate2D(latitude: 45.4294, longitude: -75.6984))
   ]
   
-  @State private var selectedLocation: Location?
-  
   var body: some View {
     Map(initialPosition: .region(region)) {
-      ForEach(locations) { location in
-        Annotation(location.name, coordinate: location.coordinate) {
-          Button(action: {
-            selectedLocation = location
-          }) {
-            Image(systemName: "star.circle.fill")
-              .foregroundColor(.blue)
-              .font(.largeTitle)
-          }
-        }
-        .annotationTitles(.hidden)
-      }
-    }
-    .edgesIgnoringSafeArea(.all)
-    .alert(item: $selectedLocation) { location in
-      Alert(title: Text(location.name), message: Text("You selected \(location.name)."), dismissButton: .default(Text("OK")))
+      MapCircle(center: locations[0].coordinate, radius: CLLocationDistance(50))
+         .foregroundStyle(.red.opacity(0.6))
+         .mapOverlayLevel(level: .aboveRoads)
+
+      MapCircle(center: locations[1].coordinate, radius: CLLocationDistance(50))
+         .foregroundStyle(.mint.opacity(0.6))
+         .mapOverlayLevel(level: .aboveLabels)
+      
+      MapPolygon(coordinates: [locations[0].coordinate,
+                               locations[2].coordinate,
+                               locations[3].coordinate,
+                               locations[4].coordinate]
+      )
+      .foregroundStyle(.red.opacity(0.6))
+      .mapOverlayLevel(level: .aboveRoads)
+      
+      MapPolyline(coordinates: [locations[1].coordinate, locations[0].coordinate])
+        .stroke(.blue, lineWidth: 5)
     }
   }
 }
